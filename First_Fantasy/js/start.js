cbda919931b578635e6a7a1fc38e01a434b1a780
ANDERPIG36 <andreaarinci4@gmail.com>
2025-10-15 10:06:15 +0200

    iniziata pagina start

---
+++ First_Fantasy/js/start.js
@@ -0,0 +1,7 @@
+const immagini = ["../img/start_1.png", "../img/start_2.png", "../img/start_3.png", "../img/start_4.png"];
+let indice = 0;
+const img = document.getElementById("start_img");
+
+img.addEventListener("click", () => {
+
+});
