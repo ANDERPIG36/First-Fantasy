c0b4dac8d49b0c07293b5645f2f32bc8a9dfdac7
ANDERPIG36 <andreaarinci4@gmail.com>
2025-10-16 22:33:47 +0200

    Finite pagine del menu(index), start e coward ending eccetto le immagini degli eventi

--- First_Fantasy/js/coward_ending.js
+++ First_Fantasy/js/coward_ending.js
@@ -1,9 +1,10 @@
-const immagini = [];
+const immagini = ["../img/endings/coward/coward_1.png", "../img/endings/coward/coward_2.png", "../img/endings/coward/coward_3.png", "../img/endings/coward/coward_4.png"];
 let i = 0;
 let j = -1;
 let monologues = [];
 const img = document.getElementById("coward_ending_img");
 const textBox = document.getElementById("text-box");
+const buttonBox = document.getElementById("button-box");
 const monologueText = document.getElementById("monologue_text");
 
 fetch('../txt/coward_ending.json')
@@ -15,7 +16,7 @@ fetch('../txt/coward_ending.json')
         console.error('Errore nel caricamento dei testi', error);
     });
 
-img.addEventListener("click", () => { //Quando clicci sull' pulsante avvia una funzione "monouso" creata sul momento che porta al avanzamento delle immagini e smette di esistere appena finisce la funzione
+document.body.addEventListener("click", () => { //Quando clicci sull' pulsante avvia una funzione "monouso" creata sul momento che porta al avanzamento delle immagini e smette di esistere appena finisce la funzione
     i++;
     if (i < immagini.length) {
         img.src = immagini[i];
@@ -23,9 +24,17 @@ img.addEventListener("click", () => { //Quando clicci sull' pulsante avvia una f
         j++;
         if (j < monologues.length) {
             textBox.classList.remove("hidden");
+            img.classList.remove("base_size");
+            img.classList.add("shrink1");
+            textBox.classList.add("box1");
             monologueText.textContent = monologues[j];
         } else {
-
+            buttonBox.classList.remove("hidden");
+            buttonBox.classList.add("button-box-showed");
+            img.classList.remove("shrink1");
+            img.classList.add("shrink2");
+            textBox.classList.remove("box1");
+            textBox.classList.add("box2");
         }
     }
 });
