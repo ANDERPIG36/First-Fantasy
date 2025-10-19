406198633e5a12284b94f57a5eb2db968df35e30
ANDERPIG36 <andreaarinci4@gmail.com>
2025-10-19 10:42:44 +0200

    aggiunti tutti gli html delle stanze, boss e finali

--- First_Fantasy/js/dungeon/dungeon1.js
+++ First_Fantasy/js/dungeon/dungeon1.js
@@ -1,6 +1,12 @@
+const personaggio = document.getElementById("giocatore_dungeon1");
+const monologueText = document.getElementById("dungeon1_text");
+const porte = document.querySelectorAll(".porta");
 let monologues = [];
-const textBox = document.getElementById("dungeon1_text");
-let i;
+let i=-1;
+
+porte.forEach(porta => {
+    porta.style.pointerEvents = "none";
+});
 
 fetch('../../txt/dungeon/dungeon1.json')
     .then(response => response.json())
@@ -11,11 +17,14 @@ fetch('../../txt/dungeon/dungeon1.json')
         console.error('Errore nel caricamento dei testi', error);
     });
 
-    document.body.addEventListener("click", () => { 
+document.body.addEventListener("click", () => { 
     i++;
     if (i < monologues.length) {
         monologueText.textContent = monologues[i];
     } else {
-    
+        personaggio.classList.add("invisible");
+        porte.forEach(porta => {
+            porta.style.pointerEvents = "auto";
+        });
     }
     });
